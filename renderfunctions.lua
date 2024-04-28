6ccb16e09040ffc3cbcafbad7f8e5fd32c945229
Gvuj <111720271+Gvuj@users.noreply.github.com>
2024-04-27 22:08:03 -0700

    Update renderfunctions.lua

--- renderfunctions.lua
+++ renderfunctions.lua
@@ -225,15 +225,6 @@ function RenderFunctions:Announcement(tab)
 	return announceframe
 end
 
-RenderFunctions:Announcement({Text = 'hi, just testing new annc system ok', Duration = 5})
-local function playerfromID(id) -- players:GetPlayerFromUserId() didn't work for some reason :bruh:
-    for i,v in next, players:GetPlayers() do 
-        if v.UserId == tonumber(id) then 
-            return v 
-        end
-    end
-end
-
 local function playerfromName(name)
     for i,v in next, players:GetPlayers() do 
         if v.Name:lower() == name:lower() then 
@@ -486,3 +477,6 @@ end)
 
 getgenv().RenderFunctions = RenderFunctions
 return RenderFunctions
+
+
+
